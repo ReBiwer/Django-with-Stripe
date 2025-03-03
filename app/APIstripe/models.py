f8433dd762060106cede92bbb161b62c6ba569db
vladimir <vovka1998@gmail.com>
2025-03-03 17:11:48 +0300

    created model Item

--- app/APIstripe/models.py
+++ app/APIstripe/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, db_index=True, verbose_name="Name")
+    description = models.TextField(null=True, blank=True, db_index=True, verbose_name="Description")
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name="Price")
