cdec34576e2d39e9cc0b615f8470cefa22310bec
vladimir <vovka1998@gmail.com>
2025-03-04 15:40:55 +0300

    fix bug

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-dotenv.load_dotenv(BASE_DIR.joinpath('.env'))
+dotenv.load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -29,8 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
-STRIPE_CUSTOMER = os.getenv('STRIPE_CUSTOMER')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_CUSTOMER = os.environ.get('STRIPE_CUSTOMER')
 
 # Application definition
 
