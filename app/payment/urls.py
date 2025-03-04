2ca947fa87e297e60ca171876b7840a01656c4e1
vladimir <vovka1998@gmail.com>
2025-03-04 15:39:40 +0300

    added and connected success and cancel payment templates

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import GetStripeSession
+from django.views.generic import TemplateView
+from .views import GetStripeSession, PayItem
 
 
 app_name = "payment"
 
 urlpatterns = [
     path('buy/<int:id>/', GetStripeSession.as_view(), name='get_session'),
+    path('item/<int:pk>/', PayItem.as_view(), name='pay_item'),
+    path('buy/success/', TemplateView.as_view(template_name='payment/success_payment.html'), name='success_payment'),
+    path('buy/cancel/', TemplateView.as_view(template_name='payment/cancel_payment.html'), name='cancel_payment'),
 ]
