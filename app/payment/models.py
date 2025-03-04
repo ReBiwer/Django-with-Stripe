b7b4b5cb44c78f4ccff545436d2bf8cb47c2621d
vladimir <vovka1998@gmail.com>
2025-03-04 15:40:21 +0300

    added get_session_url

--- app/payment/models.py
+++ app/payment/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Item(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, db_index=True, verbose_name="Name")
     description = models.TextField(null=True, blank=True, db_index=True, verbose_name="Description")
     price = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name="Price")
+
+    def get_session_url(self):
+        return reverse('payment:get_session', kwargs={'id': self.pk})
