30750832217c3b991108c2495fda60866c5c9bae
vladimir <vovka1998@gmail.com>
2025-03-04 14:35:34 +0300

    change url for payment

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -5,5 +5,5 @@ from .views import GetStripeSession
 app_name = "payment"
 
 urlpatterns = [
-    path('/buy/<int:id>/', GetStripeSession.as_view(), name='get_session'),
+    path('buy/<int:id>/', GetStripeSession.as_view(), name='get_session'),
 ]
