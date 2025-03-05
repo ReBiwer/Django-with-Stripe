ae9d48d869f2cba21b2aa84eb5220ab7fcc2a36d
vladimir <vovka1998@gmail.com>
2025-03-05 09:37:45 +0300

    register new model in admin

--- app/payment/admin.py
+++ app/payment/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Item
+from .models import Item, Order, Tax, Discount
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
@@ -8,4 +8,25 @@ class ItemAdmin(admin.ModelAdmin):
         "name",
         "description",
         "price",
+    ]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "items",
+        "discount",
+        "tax",
+    ]
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = [
+        "stripe_id",
+    ]
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = [
+        "stripe_id",
     ]
