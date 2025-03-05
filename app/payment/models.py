1ad47e2965b788808cbb96bb497740f45f745f3a
vladimir <vovka1998@gmail.com>
2025-03-05 10:13:01 +0300

    updated models

--- app/payment/models.py
+++ app/payment/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False, db_index=True, verbose_name="Name")
     description = models.TextField(null=True, blank=True, db_index=True, verbose_name="Description")
     price = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name="Price")
-    currency = models.CharField(max_length=3, choices=[
+    currency = models.CharField(max_length=3, default='USD', choices=[
         ('usd', 'USD'),
         ('eur', 'EUR')
     ])
@@ -17,8 +17,8 @@ class Item(models.Model):
 
 class Order(models.Model):
     items = models.ManyToManyField('Item')
-    discount = models.ForeignKey('Discount', null=True, on_delete=models.SET_NULL)
-    tax = models.ForeignKey('Tax', null=True, on_delete=models.SET_NULL)
+    discount = models.ForeignKey('Discount', null=True, blank=True, on_delete=models.SET_NULL)
+    tax = models.ForeignKey('Tax', null=True, blank=True, on_delete=models.SET_NULL)
 
 class Discount(models.Model):
     stripe_id = models.CharField(max_length=50)
